fcafca6a1953e0a470327f3bad3f3b64ec301d6e
mpatino117 <mpatino117@gmail.com>
2017-02-12 21:23:29 -0700

    first commit

---
+++ public/app.js
@@ -0,0 +1,90 @@
+const TimeDashboard = React.createClass({
+  render: function(){
+    return(
+      <div className="ui three column centered grid">
+        <div className="column">
+          <EditableTimerList />
+          <ToggleableTimerForm
+            isOpen={true}
+          />
+        </div>
+      </div>
+    );
+  },
+});
+
+const EditableTimerList = React.createClass({
+  render:function(){
+    return(
+      <div id="timers">
+        <EditableTimer
+          title='Learn React'
+          project='Web Domination'
+          elapsed='89235'
+          runningSince={null}
+          editFormOpen={false}
+        />
+        <EditableTimer
+          title='Learn Extreme Ironing'
+          project='World Domination'
+          elapsed='2222'
+          runningSince={null}
+          editFormOpen={false}
+        />
+        </div>
+
+    );
+  },
+});
+
+const EditableTimer = React.createClass({
+  render: function(){
+    if(this.props.editFormOpen){
+          return(
+            <TimerForm
+            title={this.props.title}
+            project={this.props.project}
+            />
+          );
+        } else {
+          return(
+            <Timer
+            title={this.props.title}
+            project={this.props.project}
+            elapsed={this.props.elapsed}
+            runningSince={this.props.runningSince}
+            />
+          );
+        }
+      },
+    });
+
+const TimerForm = React.createClass({
+    render: function() {
+      const submitText = this.props.title ? 'Update' : 'Create';
+      return(
+        <div className="ui centered card">
+          <div className="content">
+            <div className="ui form">
+              <div className="field">
+                <label> Title </label>
+                <input type="text" defaultValue={this.props.title} />
+              </div>
+              <div className="field">
+                <label> Project </label>
+                <input type="text" defaultValue={this.props.project} />
+              </div>
+              <div className="ui two bottom attached buttons">
+                <button className="ui basic blue button">
+                  {submitText}
+                </button>
+                <button className="ui basic red button">
+                  Cancel
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    },
+});
